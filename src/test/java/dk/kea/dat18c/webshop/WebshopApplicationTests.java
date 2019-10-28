73f086c1085dc722760d85f6afb0b8c28843187c
efjohannsen <efjohannsen@gmail.com>
2019-10-28 09:51:39 +0100

    start webshop dev

---
+++ src/test/java/dk/kea/dat18c/webshop/WebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.dat18c.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
